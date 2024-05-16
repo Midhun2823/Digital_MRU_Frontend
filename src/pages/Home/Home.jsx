2a29272ab7ac2336d2aefb9a8c9627642b76d1f6
Midhun2823 <132231878+Midhun2823@users.noreply.github.com>
2024-05-16 12:26:33 +0530

    Update Home.jsx -1

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Header from '../../Components/Header/Header'
-import { Navigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
   
