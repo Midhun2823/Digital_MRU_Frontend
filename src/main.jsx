7fb6da3c4aa3beac907a447cc6dfaececfcfde64
Midhun2823 <132231878+Midhun2823@users.noreply.github.com>
2024-05-16 12:48:00 +0530

    Update main.jsx -3

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,12 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import ItemContextProvider from "./Context/ItemContext";
-import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   
-  <BrowserRouter>
+  
     <ItemContextProvider>
       <App />
     </ItemContextProvider>
-  </BrowserRouter>
+  
 );
